da8fb49715bd8219e2129ebedddb8753b918e9dc
Ardian Tirta <ardiantirtaa@gmail.com>
2020-01-29 17:22:10 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+import "./question.dart";
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -30,30 +32,11 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text('Drawer Header'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                title: Text('Item 1'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Item 2'),
-                onTap: () {},
-              )
-            ],
-          ),
-        ),
         body: Column(
           children: <Widget>[
-            Text(questions[_questionIndex]),
+            Question(
+              questions[_questionIndex],
+            ),
             RaisedButton(
               child: Text("Answer 1"),
               onPressed: answerQuestion,
