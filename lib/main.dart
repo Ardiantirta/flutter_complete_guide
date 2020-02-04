35c11c566151ab47dffdff14ebacd0ca44078770
Ardian Tirta <ardiantirtaa@gmail.com>
2020-02-04 18:02:26 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 
-import "./question.dart";
-import "./answer.dart";
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,27 +14,53 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  static const questions = [
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  static const _questions = [
     {
-      "questionText": "What's your favorite color?",
-      "answers": ["Black", "Red", "Green", "White"]
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ]
     },
     {
-      "questionText": "What's your favorite animal?",
-      "answers": ["Rabbit", "Snake", "Elephan", "Lion "]
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ]
     },
     {
-      "questionText": "What's your favorite instructor?",
-      "answers": ["Max", "Max", "Max", "Max"]
+      'questionText': 'What\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ]
     }
   ];
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print("We have more questions");
     } else {
       print("No more questions");
@@ -48,19 +74,15 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: <Widget>[
-                  Question(
-                    questions[_questionIndex]["questionText"],
-                  ),
-                  ...(questions[_questionIndex]["answers"] as List<String>)
-                      .map((answer) => Answer(_answerQuestion, answer))
-                      .toList()
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
               )
-            : Center(
-                child: Text("You did it!"),
+            : Result(
+                resultScore: _totalScore,
+          resetHandler: _resetQuiz,
               ),
       ),
     );
